11d3137b95827383ae501a54a389aef58d8b38de
Matthew Greco <mgreco2112@tutanota.com>
2023-07-09 08:33:05 -0400

    populated WeaponFactory class with weapons

--- src/Logic/Weapon.java
+++ src/Logic/Weapon.java
@@ -2,11 +2,18 @@ package Logic;
 
 import java.util.List;
 
-public class Weapon extends Equipment{
+public class Weapon extends Equipment {
+
+    private boolean isGrenade = false;
     private Integer damageCode;
     private Integer maxShortRange;
     private Integer maxMediumRange;
     private Integer maxLongRange;
+
+    private Integer pointBlankDamage;
+    private Integer shortDamage;
+    private Integer mediumDamage;
+    private Integer longDamage;
 
     public Weapon(String name, Integer damageCode, Integer maxShortRange, Integer maxMediumRange, Integer maxLongRange) {
         super(name);
@@ -14,6 +21,19 @@ public class Weapon extends Equipment{
         this.maxShortRange = maxShortRange;
         this.maxMediumRange = maxMediumRange;
         this.maxLongRange = maxLongRange;
+    }
+
+    public Weapon(String name, Integer damageCode, Integer maxShortRange, Integer maxMediumRange, Integer maxLongRange, Integer pointBlankDamage, Integer shortDamage, Integer mediumDamage, Integer longDamage) {
+        super(name);
+        this.damageCode = damageCode;
+        this.maxShortRange = maxShortRange;
+        this.maxMediumRange = maxMediumRange;
+        this.maxLongRange = maxLongRange;
+        this.pointBlankDamage = pointBlankDamage;
+        this.shortDamage = shortDamage;
+        this.mediumDamage = mediumDamage;
+        this.longDamage = longDamage;
+        this.isGrenade = true;
     }
 
     public Integer getDamageCode() {
@@ -46,5 +66,53 @@ public class Weapon extends Equipment{
 
     public void setMaxLongRange(Integer maxLongRange) {
         this.maxLongRange = maxLongRange;
+    }
+
+    public Integer getPointBlankDamage() {
+        if (isGrenade) {
+            return pointBlankDamage;
+        } else {
+            return null;
+        }
+    }
+
+    public void setPointBlankDamage(Integer pointBlankDamage) {
+        this.pointBlankDamage = pointBlankDamage;
+    }
+
+    public Integer getShortDamage() {
+        if (isGrenade) {
+            return shortDamage;
+        } else {
+            return null;
+        }
+    }
+
+    public void setShortDamage(Integer shortDamage) {
+        this.shortDamage = shortDamage;
+    }
+
+    public Integer getMediumDamage() {
+        if (isGrenade) {
+            return mediumDamage;
+        } else {
+            return null;
+        }
+    }
+
+    public void setMediumDamage(Integer mediumDamage) {
+        this.mediumDamage = mediumDamage;
+    }
+
+    public Integer getLongDamage() {
+        if (isGrenade) {
+            return longDamage;
+        } else {
+            return null;
+        }
+    }
+
+    public void setLongDamage(Integer longDamage) {
+        this.longDamage = longDamage;
     }
 }
--- src/Logic/WeaponFactory.java
+++ src/Logic/WeaponFactory.java
@@ -1,5 +1,76 @@
 package Logic;
 
 public class WeaponFactory {
-    //TODO create factory class that returns created weapons
+    public static Weapon holdOutBlaster() {
+        return new Weapon("Hold-out Blaster", 3, 4, 8, 12);
+    }
+
+    public static Weapon sportingBlaster() {
+        return new Weapon("Sporting Blaster", 3, 10, 30, 120);
+    }
+
+    public static Weapon blasterPistol() {
+        return new Weapon("Blaster Pistol", 4, 10, 30, 120);
+    }
+
+    public static Weapon heavyBlasterPistol() {
+        return new Weapon("Heavy Blaster Pistol", 5, 7, 25, 50);
+    }
+
+    public static Weapon huntingBlaster() {
+        return new Weapon("Hunting Blaster", 4, 30, 100, 300);
+    }
+
+    public static Weapon blasterRifle() {
+        return new Weapon("Blaster Rifle", 5, 30, 100, 300);
+    }
+
+    public static Weapon blasterCarbine() {
+        return new Weapon("Blaster Carbine", 5, 25, 60, 250);
+    }
+
+    public static Weapon mediumRepeatingBlaster() {
+        return new Weapon("Medium Repeating Blaster", 7, 60, 150, 400);
+    }
+
+    public static Weapon heavyRepeatingBlaster() {
+        return new Weapon("Heavy Repeating Blaster", 8, 75, 200, 500);
+    }
+
+    public static Weapon crossbow() {
+        return new Weapon("Crossbow", 2, 10, 30, 50);
+    }
+
+    public static Weapon longbow() {
+        return new Weapon("Longbow", 2, 10, 30, 100);
+    }
+
+    public static Weapon blackPowderPistol() {
+        return new Weapon("Black Powder Pistol", 2, 4, 8, 12);
+    }
+
+    public static Weapon musket() {
+        return new Weapon("Musket", 3, 10, 30, 100);
+    }
+
+    public static Weapon rifle() {
+        return new Weapon("Rifle", 3, 30, 100, 300);
+    }
+
+    public static Weapon submachinegun() {
+        return new Weapon("Submachinegun", 4, 10, 50, 100);
+    }
+
+    public static Weapon wookieBowcaster() {
+        return new Weapon("Wookie Bowcaster", 4, 10, 30, 50);
+    }
+
+    public static Weapon grenade() {
+        return new Weapon("Grenade", null, 7, 20, 40, 5, 4, 3, 2);
+    }
+
+    public static Weapon thermalDetonator() {
+        return new Weapon("Thermal Detonator", null, 4, 7, 12, 10, 8, 5, 2);
+    }
 }
+
